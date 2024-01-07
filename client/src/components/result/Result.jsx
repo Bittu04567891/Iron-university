0ef5742422000e65f7aa27f643fc53cf9c9e958a
Shaketshubham276 <2021ugme023@nitjsr.ac.in>
2024-01-07 14:03:09 +0530

    1st shaket

--- client/src/components/result/Result.jsx
+++ client/src/components/result/Result.jsx
@@ -18,11 +18,7 @@ const Result = () => {
                   BEST RESULTS
                 </h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit
+                  shaket shubham
                 </p>
                 <a href="">Get A Quote</a>
               </div>
